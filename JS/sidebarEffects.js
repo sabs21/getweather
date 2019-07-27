786bba27efebfe99d24afba29a855ca7b90c673b
Nick Sabia <nms00010101@outlook.com>
2019-07-27 12:29:43 -0400

    improved readability of onloadEffects and sidebarEffects

--- JS/sidebarEffects.js
+++ JS/sidebarEffects.js
@@ -1,16 +1,3 @@
-// CSS elements are assigned to 'style' innerHTML.
-var style = document.createElement("style");
-// Then the style tag is appended to the head of the HTML document.
-var ref = document.querySelector("head");
-// Finally, we add the element to the HTML.
-ref.appendChild(style);
-// From here, we may begin editing the css of the document.
-
 document.getElementById("nav--recent").addEventListener("click", function() {
-  // Since the tri-dots will only be visible when the sidebar is closed, then
-  // we will only handle opening the side bar with the tri-dots.
-  style.innerHTML +=
-  "#recent {" +
-    "width: " + 400 + "px;" +
-  "}";
+  document.getElementById("recent").className = "";
 });
