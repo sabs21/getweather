3d4809961afdce9b85837087b7a72a15c481b8da
Nick Sabia <nms00010101@outlook.com>
2019-07-27 15:54:45 -0400

    added sidebar close button and title for recent searches

--- JS/sidebarEffects.js
+++ JS/sidebarEffects.js
@@ -1,3 +1,7 @@
 document.getElementById("nav--recent").addEventListener("click", function() {
   document.getElementById("recent").className = "";
 });
+
+document.getElementById("recent--close").addEventListener("click", function() {
+  document.getElementById("recent").className = "collapsed";
+})
