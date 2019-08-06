804fcd61d4d7d8c384bcf727b5c00e7169edb579
Nick Sabia <nms00010101@outlook.com>
2019-08-05 20:21:30 -0400

    added settings icon with sidebar

---
+++ JS/settingsEffects.js
@@ -0,0 +1,5 @@
+document.getElementById("nav--settings").addEventListener("click", function() {
+  document.getElementById("settings").className = "";
+  // it does work when clicked on
+  //window.alert("works");
+});
